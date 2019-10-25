c7d0028c78a543d6668bd27cd85431694a77240c
Gasperpaul <pavlo.borysenko@gmail.com>
2019-10-25 23:46:26 +0300

    fixed tooling

--- tools/run_jupyter.bat
+++ tools/run_jupyter.bat
@@ -1,16 +1,14 @@
 @echo off
 set VENV_DIR=..\.venv
 set CONTENT_DIR=..\content
-set PDF_FILE=P:\JupyterBook\Python
-
-start "" %PDF_FILE%.pdf
 
 if exist %VENV_DIR%\ (
     call %VENV_DIR%\Scripts\activate.bat
 ) else (
-    py -m venv %VENV_DIR%
+    python -m venv %VENV_DIR%
     call %VENV_DIR%\Scripts\activate.bat
     pip install -r requirements.txt
 )
+
 jupyter-lab --notebook-dir=%CONTENT_DIR%
 call deactivate
